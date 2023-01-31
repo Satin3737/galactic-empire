4e6057f1133c5837c7c5e168290c0fd5b567fa12
satin <satin3737@gmail.com>
2023-01-31 21:52:53 +0200

    added promo block and menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 import {isWebP} from "./modules/functions.js";
 
 isWebP();
+
+const burgerBtn = document.querySelector('[data-burger]');
+const menu = document.querySelector('[data-menu]');
+burgerBtn.addEventListener('click' , () => {
+    burgerBtn.classList.toggle('menu-open');
+    menu.classList.toggle('menu-open');
+});
